19970519365282426c4d004a54c76f1cb8140344
Ruitong Dai <3192529076@qq.com>
2026-07-30 21:19:00 +0800

    更改imu

--- src/tasks/amp_loco/config/e1/env_cfgs.py
+++ src/tasks/amp_loco/config/e1/env_cfgs.py
@@ -206,8 +206,8 @@ def e1_amp_flat_env_cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
   if play:
     twist_cmd = cfg.commands["twist"]
     assert isinstance(twist_cmd, UniformVelocityCommandCfg)
-    twist_cmd.ranges.lin_vel_x = (1.0,1.0)
-    twist_cmd.ranges.lin_vel_y = (0, 0)
-    twist_cmd.ranges.ang_vel_z = (0.5,0.5)
+    twist_cmd.ranges.lin_vel_x = (0,0)
+    twist_cmd.ranges.lin_vel_y = (0.6, 0.6)
+    twist_cmd.ranges.ang_vel_z = (0,0)
 
   return cfg
